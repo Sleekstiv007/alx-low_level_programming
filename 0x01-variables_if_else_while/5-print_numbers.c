46882241834f93947ab64bcc6913e82128276948
Sleekstiv007 <smiene13@gmail.com>
2023-10-06 21:13:09 -0700

    5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * author: steven
+ * Return: Always 0 (Success/correct)
+ */
+int main(void)
+{
+	int steven;
+
+	for (steven = 0; steven < 10; steven++)
+	{
+		printf("%d", steven);
+	}
+	printf("\n");
+	return (0);
+}
