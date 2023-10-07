cabbfd0116eb3a5f7b2db5d946331d3610b79774
Sleekstiv007 <smiene13@gmail.com>
2023-10-06 21:17:40 -0700

    6-print_numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * author: steven
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int steven;
+
+	for (steven = 48; steven < 58; steven++)
+		putchar(steven);
+	putchar('\n');
+	return (0);
+}
