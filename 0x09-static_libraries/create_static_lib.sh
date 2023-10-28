4f9ad9f893fbf7ba30f27ca382183cfda6b3eb22
Sleekstiv007 <smiene13@gmail.com>
2023-10-27 18:14:48 -0700

    update task

--- 0x09-static_libraries/create_static_lib.sh
+++ 0x09-static_libraries/create_static_lib.sh
@@ -1,4 +1,17 @@
 #!/bin/bash
-gcc -Wall -pendantic -Werror -Wextra -c *.c
-ar -rc liball.a *.o
+
+# Compile each .c file in the current directory to an object file
+for file in *.c
+do
+  gcc -Wall -pedantic -Werror -Wextra -c "$file" -o "${file%.c}.o"
+done
+
+# Create a static library (liball.a) from all the object files
+ar rcs liball.a *.o
+
+# Run ranlib on the library (optional)
 ranlib liball.a
+
+# Clean up: remove the individual object files
+rm -f *.o
+
