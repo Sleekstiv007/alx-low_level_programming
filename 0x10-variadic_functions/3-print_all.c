6840d6b0102ef3772b8e5e73364ed206f88ae05c
Sleekstiv007 <smiene13@gmail.com>
2023-11-09 14:20:38 -0800

    updated

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -14,7 +14,7 @@ void print_all(const char * const format, ...)
 	va_start(valist, format);
 	while (format && format[i])
 	{
-		y = 0;
+		j = 0;
 		while (t_arg[j])
 		{
 			if (format[i] == t_arg[j] && k)
